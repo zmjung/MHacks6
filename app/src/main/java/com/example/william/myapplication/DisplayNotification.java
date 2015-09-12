be33026ff6e385f89caebcf009675d9a4fcc2149
William Su <williamsu@gatech.edu>
2015-09-12 17:34:21 -0400

    added notification

---
+++ app/src/main/java/com/example/william/myapplication/DisplayNotification.java
@@ -0,0 +1,15 @@
+package com.example.william.myapplication;
+
+/**
+ * Created by Admin on 9/12/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayNotification extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
--- app/src/main/java/com/example/william/myapplication/MainActivity.java
+++ app/src/main/java/com/example/william/myapplication/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.william.myapplication;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -388,6 +393,21 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNo, null, message, null, null);
         Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+    }
+    public void createNotification(View view) {
+
+        Intent intent = new Intent(this, DisplayNotification.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject").setSmallIcon(R.drawable.icon)
+                .setContentIntent(pIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, noti);
+
     }
 
 }
