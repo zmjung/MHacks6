ad61dbd48b5e4c1a64ce36923eabd39a6ddf383d
Zhijian <zhijianli@gatech.edu>
2015-09-12 09:49:48 -0400

    resolve conflicts

---
+++ app/src/main/java/com/example/william/myapplication/AddFriendMenu.java
@@ -0,0 +1,41 @@
+package com.example.william.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddFriendMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void save(View view) {
+        EditText name = (EditText)findViewById(R.id.nameSpace);
+        EditText phoneNumber = (EditText)findViewById(R.id.phoneNumberSpace);
+        MainActivity.addFriend(name.getText().toString(),
+                Integer.parseInt(phoneNumber.getText().toString()));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/william/myapplication/Friend.java
@@ -0,0 +1,14 @@
+package com.example.william.myapplication;
+
+/**
+ * Created by Zhijian on 9/12/2015.
+ */
+public class Friend {
+    private int number;
+    private String name;
+
+    public Friend(String name, int number) {
+        this.number = number;
+        this.name = name;
+    }
+}
