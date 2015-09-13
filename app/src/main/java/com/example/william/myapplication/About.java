ae1d1e73cc065fb4025f84de88a9c193da040e88
James <jamesjung1123@gmail.com>
2015-09-13 09:07:42 -0400

    about page

--- app/src/main/java/com/example/william/myapplication/About.java
+++ app/src/main/java/com/example/william/myapplication/About.java
@@ -1,25 +1,22 @@
 package com.example.william.myapplication;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-/**
- * Created by zmjung on 9/13/15.
- */
 public class About extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_about);
-        // idk what im doing - its like 5 am atm
+        setContentView(R.layout.activity_about);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_remove_friend, menu);
+        getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
 
