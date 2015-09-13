b570be68203e09546bf0b74cd3858f2b2722202f
William Hsu <william7hsu@gmail.com>
2015-09-13 09:07:35 -0400

    intent

--- app/src/main/java/com/example/william/myapplication/MainActivity.java
+++ app/src/main/java/com/example/william/myapplication/MainActivity.java
@@ -3,13 +3,11 @@ package com.example.william.myapplication;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -272,6 +270,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .setSmallIcon(R.drawable.mr_ic_audio_vol)
                 .setAutoCancel(true)
                 .setVisibility(1).build();
+//        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//// Adds the back stack for the Intent (but not the Intent itself)
+//        stackBuilder.addParentStack(MainActivity.class);
+//// Adds the Intent that starts the Activity to the top of the stack
+//        stackBuilder.addNextIntent(resultIntent);
+//        PendingIntent resultPendingIntent =
+//                stackBuilder.getPendingIntent(
+//                        0,
+//                        PendingIntent.FLAG_UPDATE_CURRENT
+//                );
+//        mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(001, notification);
