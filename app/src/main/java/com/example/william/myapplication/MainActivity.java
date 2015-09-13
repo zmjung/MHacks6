c4b63fd37ec3ee70a2ce2252f9e15f0d3a4d97c4
William Hsu <william7hsu@gmail.com>
2015-09-13 00:08:34 -0400

    notification

--- app/src/main/java/com/example/william/myapplication/MainActivity.java
+++ app/src/main/java/com/example/william/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.william.myapplication;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
@@ -262,23 +261,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     private void Notify(){
-        // Set the icon, scrolling text and timestamp
-        //We get a reference to the NotificationManager
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        String MyText = "Reminder";
-        Notification mNotification = new Notification(R.drawable.mr_ic_audio_vol, MyText, System.currentTimeMillis() );
-        //The three parameters are: 1. an icon, 2. a title, 3. time when the notification appears
-
-        String MyNotificationTitle = "Medicine!";
-        String MyNotificationText  = "Don't forget to take your medicine!";
-
-        Intent MyIntent = new Intent(Intent.ACTION_VIEW);
-        PendingIntent StartIntent = PendingIntent.getActivity(getApplicationContext(),0,MyIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-        //A PendingIntent will be fired when the notification is clicked. The FLAG_CANCEL_CURRENT flag cancels the pendingintent
-        
-        int NOTIFICATION_ID = 1;
-        notificationManager.notify(NOTIFICATION_ID , mNotification);
+        Notification notification  = new Notification.Builder(this)
+                .setCategory(Notification.CATEGORY_MESSAGE)
+                .setContentTitle("test")
+                .setContentText("plswork")
+                .setSmallIcon(R.drawable.mr_ic_audio_vol)
+                .setAutoCancel(true)
+                .setVisibility(1).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(001, notification);
     }
     public static void addFriend(String name, String number) {
         myFirebaseRef.child("FriendsList").child(deviceNumber).child(number).setValue(name);
